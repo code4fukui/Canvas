b8c7e6181dd79f05c91fd434cd69578e43e38f00
Taisuke Fukuno <fukuno@jig.jp>
2021-09-26 08:18:20 +0900

    Initial commit

---
+++ Canvas.js
@@ -0,0 +1,23 @@
+import { Base64 } from "https://code4fukui.github.io/Base64/Base64.js";
+
+const encodeImage = (img, mimeType = "image/jpeg", quality = 0.9) => {
+  const canvas = document.createElement("canvas");
+  canvas.width  = img.width;
+  canvas.height = img.height;
+  const g = canvas.getContext("2d");
+  g.putImageData(img, 0, 0);
+  const d = canvas.toDataURL(mimeType, quality);
+  return Base64.decode(d.substring(d.indexOf(",") + 1));
+};
+
+const Canvas = {};
+
+Canvas.encodeJPEG = (imgdata, quality = 0.9) => {
+  return encodeImage(imgdata, "image/jpeg", quality)
+};
+Canvas.encodePNG = (imgdata) => {
+  return encodeImage(imgdata, "image/png", quality)
+};
+
+export { Canvas };
+
